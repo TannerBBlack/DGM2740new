b4171001f1c041d4412edb813e86038d46057553
Tanner Black <tbb777@gmail.com>
2021-02-24 12:10:46 -0700

    fixed colors, cards, and icons

--- Module12/js/scripts.js
+++ Module12/js/scripts.js
@@ -1,5 +1,5 @@
 // what is the path to the JSON file?
-const apiURL = "../hoteldata.json";
+const apiURL = "/Module12/hoteldata.json";
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
@@ -7,16 +7,50 @@ fetch(apiURL)
   .then((myList) => {
     //Once it comes back, display it to the console.
     console.log(myList);
+
+    for (i = 0; i < myList.length; i++) {
     let myImageTag = document.createElement("img");
-    myImageTag.src = myList[0].photo;
-    
-    let myCaptionTag = document.createElement("figcation");
-    myCaptionTag.textContent = myList[0].name;
-    
+    myImageTag.src = myList[i].photo;
+
+    let myCaptionTag = document.createElement("figcaption");
+    myCaptionTag.textContent = myList[i].name;
+
     let myFigureTag = document.createElement("figure");
     myFigureTag.appendChild(myImageTag);
     myFigureTag.appendChild(myCaptionTag);
+
+    let theIconLeftTag = document.createElement("ion-icon");
+    theIconLeftTag.name = "car";
+
+    let theAddress1Tag = document.createElement("p");
+    theAddress1Tag.textContent = myList[i].address[0];
+
+    let theAddress2Tag = document.createElement("p");
+    theAddress2Tag.textContent = myList[i].address[1];
+
+    let theIconRightTag = document.createElement("ion-icon");
+    theIconRightTag.name = "call";
+
+    let thePhoneTag = document.createElement("p");
+    thePhoneTag.textContent = myList[i].phone;
     
-    
-    document.getElementById('myCards').appendChild(myCaptionTag);
+    let theSpanLeftTag = document.createElement("span");
+    theSpanLeftTag.appendChild(theIconLeftTag);
+    theSpanLeftTag.appendChild(theAddress1Tag);
+    theSpanLeftTag.appendChild(theAddress2Tag);
+
+    let theSpanRightTag = document.createElement("span");
+    theSpanRightTag.appendChild(theIconRightTag);
+    theSpanRightTag.appendChild(thePhoneTag);
+
+    let theCardDiv = document.createElement("div");
+    theCardDiv.appendChild(theSpanLeftTag);
+    theCardDiv.appendChild(theSpanRightTag);
+
+    let theCardSection = document.createElement("section");
+    theCardSection.appendChild(myFigureTag);
+    theCardSection.appendChild(theCardDiv);
+
+    document.getElementById('myCards').appendChild(theCardSection);
+    }
 }); //end of "then" fat arrow function
